e2094d369e5b22e44b59d7a468c50f06aa64e98c
tnordnes <129512758+tnordnes@users.noreply.github.com>
2023-04-12 21:49:17 +0200

    Renamed name

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name = "blog_new"
+  name = "blog"
 
   vpc_id = data.aws_vpc.default.id
 
